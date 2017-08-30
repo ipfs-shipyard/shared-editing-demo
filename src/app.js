61c84f100ff2f1d3abe5a085a06fe28c8a2b0bdf
David Dias <daviddias.p@gmail.com>
2017-08-30 12:20:08 +0200

    fix: only start yjs after IPFS is ready

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
 const IPFS = require('ipfs')
+const Y = require('yjs')
+require('y-memory')(Y)
+require('y-array')(Y)
+require('y-text')(Y)
+require('y-ipfs-connector')(Y)
+
+function repo () {
+  return 'ipfs/yjs-demo/' + Math.random()
+}
 
 const ipfs = new IPFS({
   repo: repo(),
@@ -11,30 +20,20 @@ ipfs.once('ready', () => ipfs.id((err, info) => {
   if (err) { throw err }
 
   console.log('IPFS node ready with address ' + info.id)
+
+  Y({
+    db: {
+      name: 'memory'
+    },
+    connector: {
+      name: 'ipfs',
+      room: 'ipfs-yjs-demo',
+      ipfs: ipfs
+    },
+    share: {
+      textfield: 'Text'
+    }
+  }).then((y) => {
+    y.share.textfield.bind(document.getElementById('textfield'))
+  })
 }))
-
-const Y = require('yjs')
-require('y-memory')(Y)
-require('y-array')(Y)
-require('y-text')(Y)
-require('y-ipfs-connector')(Y)
-
-Y({
-  db: {
-    name: 'memory'
-  },
-  connector: {
-    name: 'ipfs',
-    room: 'ipfs-yjs-demo',
-    ipfs: ipfs
-  },
-  share: {
-    textfield: 'Text'
-  }
-}).then((y) => {
-  y.share.textfield.bind(document.getElementById('textfield'))
-})
-
-function repo () {
-  return 'ipfs/yjs-demo/' + Math.random()
-}
